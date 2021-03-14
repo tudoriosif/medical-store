20a8c9e0d58e4b02f1df99c8d1db74dc4be461ef
Iosif Tudor Andrei <tudor_iosif@yahoo.com>
2021-03-14 22:03:06 +0200

    first push

---
+++ src/main/java/com/tdapp/reactspringapp/ReactSpringApplication.java
@@ -0,0 +1,14 @@
+package com.tdapp.reactspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ReactSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringApplication.class, args);
+    }
+
+}
